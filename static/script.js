7ff68069ab0892ac69a5e4b54fde86cde2edec3a
Samuele Mattiuzzo <samumatt@gmail.com>
2014-08-13 23:28:00 +0100

    Polishing js more

--- static/script.js
+++ static/script.js
@@ -1,6 +1,5 @@
 /* Globals */
 var i = 0,
-    has_cursor = false,
     in_progress = false,
     marvin_i = 0,
     current,
@@ -11,18 +10,17 @@ function swapCursor(show_cursor){
     var marvin = document.getElementById('marvinsay'),
         cursor = document.getElementById('cursor');
 
-    if (show_cursor === true) {
-        marvin.style.display = 'none';
-        cursor.style.display = 'block';
-    } else {
-        marvin.style.display = 'block';
-        cursor.style.display = 'none';
-    }
+    var marvin_display = (show_cursor === true) ? 'none' : 'block',
+        cursor_display = (show_cursor === false) ? 'none' : 'block';
+    
+    marvin.style.display = marvin_display;
+    cursor.style.display = cursor_display;
 }
 
 function type() {
     text = (i < current.length) ?
             current.slice(0, ++i) : current;
+    
     if (current === 'cursor' || text === current) {
         // resets things, set index to next iteration
         // marks current as '' and type the last word
@@ -30,17 +28,17 @@ function type() {
         marvin_i = (marvin_i === says.length-1) ? 0 : marvin_i + 1;
         i = 0;
 
-        to_print = (current !== 'cursor') ? text : '';
+        var to_print = (current !== 'cursor') ? text : '';
         document.getElementById('marvinsay').innerHTML = to_print;
         return;
     }
+
     document.getElementById('marvinsay').innerHTML = text;
     setTimeout(type, 80);
 }
 
 function marvin() {
     if (in_progress === false){
-        in_progress = true;
         if (current === 'cursor') {
             swapCursor(true);
             setTimeout(function(){
@@ -50,6 +48,7 @@ function marvin() {
                 in_progress = false;
             }, 5000);
         } else {
+            in_progress = true;
             swapCursor(false);
             current = says[marvin_i];
             type();
