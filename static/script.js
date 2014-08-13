24d0931eb703859be34e57cf641762cea86a52e3
Samuele Mattiuzzo <samumatt@gmail.com>
2014-08-13 02:08:37 +0100

    Cleaned js code. Improved infinite looping

--- static/script.js
+++ static/script.js
@@ -12,61 +12,56 @@ function changeString(){
     marvin.style.display = 'none';
     marvin.innerHTML = '';
     marvin.style.display = 'block';
-};
+}
 
 function swapCursor(show_cursor){
     var marvin = document.getElementById('marvinsay'),
         cursor = document.getElementById('cursor');
 
-    if (show_cursor == true) {
+    if (show_cursor === true) {
         marvin.style.display = 'none';
         cursor.style.display = 'inline-block';
     } else {
         marvin.style.display = 'block';
         cursor.style.display = 'none';
     }
-};
+}
 
 function type() {
-        text = current.slice(0, ++i);
+        text = (i < current.length) ?
+                current.slice(0, ++i) : current;
         if (current === 'cursor' || text === current) {
             // resets things, set index to next iteration
             // marks current as '' and type the last word
             in_progress = false;
-            if (marvin_i >= says.length){
-                marvin_i = 0;
-            } else {
-                marvin_i += 1;
-            }
+            marvin_i = (marvin_i == says.length) ? 0 : marvin_i + 1;
             i = 0;
-            if (current !== 'cursor') {
-                document.getElementById('marvinsay').innerHTML = text;
-            } else {
-                document.getElementById('marvinsay').innerHTML = '';
-            }
+
+            to_print = (current !== 'cursor') ? text : '';
+            document.getElementById('marvinsay').innerHTML = to_print;
             return;
         }
         document.getElementById('marvinsay').innerHTML = text;
         setTimeout(type, 80);
 
-};
+}
 
 function marvin() {
-    if (in_progress == false){
+    if (in_progress === false){
         in_progress = true;
         if (current === 'cursor') {
             swapCursor(true);
             setTimeout(function(){
                 swapCursor(false);
                 current = '';
-                marvin_i += 1;
+                marvin_i = (marvin_i === says.length) ? 0 : marvin_i + 1;
                 in_progress = false;
             }, 5000);
         } else {
             swapCursor(false);
-            current = says[marvin_i]
+            current = says[marvin_i];
             type();
             return;
         }
     }
-};
+}
